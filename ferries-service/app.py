87da88db6bdbdccee7e1d4f71d01906ea5e73591
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-08-08 20:01:52 +0700

    feat: setup project to run locally with docker

---
+++ ferries-service/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Load .env variables
+load_dotenv()
+
+# Load config from env
+PORT = int(os.getenv("PORT", 5001))
+DB_URL = os.getenv("FERRYS_DB_URL")
+FERRY_API_KEY = os.getenv("EXTERNAL_FERRY_API_KEY")
+
+# Initialize FastAPI
+app = FastAPI()
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "port": PORT,
+        "db_url_present": bool(DB_URL),
+        "api_key_present": bool(FERRY_API_KEY)
+    }
