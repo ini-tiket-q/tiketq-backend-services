40072fba7f5843d432883df324ccd4b483b257ae
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-08-22 09:56:18 +0700

    fix: edit booking data needed

--- ferries-service/adapters/external_api.py
+++ ferries-service/adapters/external_api.py
@@ -1,32 +1,26 @@
-import os
-import requests
 import uuid
 from domain.models import Passenger
 
-# FERRY_BASE_URL = os.getenv("EXTERNAL_FERRY_API_URL")
-# FERRY_API_KEY = os.getenv("EXTERNAL_FERRIES_API_KEY")
-
-# def create_ferry_booking(schedule_id: str, passengers: list):
-#     """
-#     Call external ferry API to create booking
-#     """
-#     url = f"{FERRY_BASE_URL}/Agent/Booking/Bookings"
-#     headers = {"Authorization": f"Bearer {FERRY_API_KEY}"}
-#     payload = {
-#         "schedule_id": schedule_id,
-#         "passengers": [p.dict() for p in passengers]
-#     }
-#     res = requests.post(url, json=payload, headers=headers)
-#     res.raise_for_status()
-#     return res.json()
+def calculate_price(passengers: list[Passenger]) -> float:
+    """
+    Mock pricing rules:
+    Adult = Rp170.000
+    Child = Rp70.000
+    """
+    total = 0
+    for p in passengers:
+        if p.type.lower() == "adult":
+            total += 170_000
+        elif p.type.lower() == "child":
+            total += 70_000
+        else:
+            total += 100_000  # default price jika tidak dikenali
+    return total
 
 
 def create_ferry_booking(schedule_id: str, passengers: list[Passenger]):
-    """
-    Mock external ferry API booking
-    """
     booking_id = str(uuid.uuid4())
-    total_price = 150.0 * len(passengers)  # mock harga Rp150k per orang
+    total_price = calculate_price(passengers)
 
     return {
         "booking_id": booking_id,
--- ferries-service/domain/models.py
+++ ferries-service/domain/models.py
@@ -1,24 +1,31 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List, Optional
+from datetime import date
 
 class Passenger(BaseModel):
-    name: str
-    age: int
+    type: str                   # "Adult" atau "Child"
+    title: str                  # "Mr", "Mrs", "Ms"
+    name: str                   # Nama sesuai passport
+    passport_no: str
     nationality: str
-    gender: str
+    issuing_country: str
+    dob: date                   # Date of Birth
+    passport_expiry: date
+    passport_issue: date
+
+class BookingRequirements(BaseModel):
+    email: EmailStr
+    confirm_email: EmailStr
+    mobile_phone: str
+    whatsapp_no: Optional[str]
 
 class FerryBookingRequest(BaseModel):
     schedule_id: str
     passengers: List[Passenger]
+    requirements: BookingRequirements
 
 class FerryBookingResponse(BaseModel):
     booking_id: str
     status: str
     total_price: float
     message: str
-
-class FerryTransaction(BaseModel):
-    transaction_id: str
-    booking_id: str
-    amount: float
-    status: str   # pending / paid / cancelled
