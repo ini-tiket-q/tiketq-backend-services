37a93d79169ed39ceb37d6c46d974fc6b4061751
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-09-21 19:28:15 +0700

    chore: delete mock files

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -11,7 +11,7 @@ load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
 
 app = FastAPI()
 
-# ✅ Setup CORS
+# Setup CORS
 origins = [
     "http://localhost:3000",   # Next.js dev
     "http://127.0.0.1:3000",   # alternatif local dev
